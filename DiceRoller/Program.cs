bae42554d31182e6bc11861605ac868ab8a9cf4c
sabrasnider <sabra.snider@s2shape.com>
2021-03-26 22:47:26 -0400

    Add user instructions to appear on program startup

--- DiceRoller/Program.cs
+++ DiceRoller/Program.cs
@@ -6,7 +6,9 @@ namespace DiceRoller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I have these dice available: D4, D6, D8, D10, D12, D20, D100");
+            Console.WriteLine("Enter the number corresponding to the dice you want to roll. (e.g. 4 for D4, 20 for D20)");
+            int dieToRoll = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
