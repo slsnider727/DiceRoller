3dc480f46410da440a1ddcb26e95ccaa471f691c
sabrasnider <sabra.snider@s2shape.com>
2021-03-26 22:54:58 -0400

    Add basic functionality for D4, D6, D8

--- DiceRoller/Program.cs
+++ DiceRoller/Program.cs
@@ -10,5 +10,49 @@ namespace DiceRoller
             Console.WriteLine("Enter the number corresponding to the dice you want to roll. (e.g. 4 for D4, 20 for D20)");
             int dieToRoll = Convert.ToInt32(Console.ReadLine());
         }
+
+        public class Dice
+        {
+            public int GetD4Result()
+            {
+                Random random = new Random();
+                int randomInt = random.Next(1, 4);
+                return randomInt;
+            }
+
+            public int GetD6Result()
+            {
+                Random random = new Random();
+                int randomInt = random.Next(1, 6);
+                return randomInt;
+            }
+
+            public int GetD8Result()
+            {
+                Random random = new Random();
+                int randomInt = random.Next(1, 8);
+                return randomInt;
+            }
+
+            public int GetD10Result()
+            {
+                Random random = new Random();
+            }
+
+            public int GetD12Result()
+            {
+                Random random = new Random();
+            }
+
+            public int GetD20Result()
+            {
+                Random random = new Random();
+            }
+
+            public int GetD100Result()
+            {
+                Random random = new Random();
+            }
+        }
     }
 }
