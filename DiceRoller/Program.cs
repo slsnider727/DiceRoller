a5fac3069788802077acf6b7117805748aece9fa
sabrasnider <sabra.snider@s2shape.com>
2021-03-27 13:52:36 -0400

    refactor dice roll to allow for any size dice and remove redundant methods

--- DiceRoller/Program.cs
+++ DiceRoller/Program.cs
@@ -7,91 +7,22 @@ namespace DiceRoller
         static void Main(string[] args)
         {
             Dice dice = new Dice();
-            Console.WriteLine("I have these dice available: D4, D6, D8, D10, D12, D20, D100");
+            Console.WriteLine("I have so many dice! I can roll anything!");
             Console.WriteLine("Enter the number corresponding to the dice you want to roll. (e.g. 4 for D4, 20 for D20)");
             int dieToRoll = Convert.ToInt32(Console.ReadLine());
-            int result = 0;
-            switch (dieToRoll)
-            {
-                case 4:
-                    result =  dice.GetD4Result();
-                    break;
-                case 6:
-                    result = dice.GetD6Result();
-                    break;
-                case 8:
-                    result = dice.GetD8Result();
-                    break;
-                case 10:
-                    result = dice.GetD10Result();
-                    break;
-                case 12:
-                    result = dice.GetD12Result();
-                    break;
-                case 20:
-                    result = dice.GetD20Result();
-                    break;
-                case 100:
-                    result = dice.GetD20Result();
-                    break;
-                default:
-                    Console.WriteLine("I'm sorry, I don't have any dice like that.");
-                    break;
-            }
+            int result = dice.GetDiceResult(dieToRoll);
             Console.WriteLine($"I rolled a D{dieToRoll} and the result is {result}.");
         }
 
         public class Dice
         {
-            //Separate methods written intentionally to indicate different dice and leave room for special features for individual dice.
-            public int GetD4Result()
+            public int GetDiceResult(int numberOfSides)
             {
                 Random random = new Random();
-                int randomInt = random.Next(1, 5);
+                int randomInt = random.Next(1, (numberOfSides + 1));
                 return randomInt;
             }
 
-            public int GetD6Result()
-            {
-                Random random = new Random();
-                int randomInt = random.Next(1, 7);
-                return randomInt;
-            }
-
-            public int GetD8Result()
-            {
-                Random random = new Random();
-                int randomInt = random.Next(1, 9);
-                return randomInt;
-            }
-
-            public int GetD10Result()
-            {
-                Random random = new Random();
-                int randomInt = random.Next(1, 11);
-                return randomInt;
-            }
-
-            public int GetD12Result()
-            {
-                Random random = new Random();
-                int randomInt = random.Next(1, 13);
-                return randomInt;
-            }
-
-            public int GetD20Result()
-            {
-                Random random = new Random();
-                int randomInt = random.Next(1, 21);
-                return randomInt;
-            }
-
-            public int GetD100Result()
-            {
-                Random random = new Random();
-                int randomInt = random.Next(1, 101);
-                return randomInt;
-            }
         }
     }
 }
